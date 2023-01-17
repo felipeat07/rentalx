7ca933626b5618c75480024a979a9aa33d8ac2ea
Felipe Tavares <felipeat07@gmail.com>
2023-01-17 17:19:29 -0300

    documentando com swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from "express";
+
+import swaggerUi from "swagger-ui-express"
+import swargerFile from "./swagger.json"
+
 import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swargerFile))
 
 app.use(router)
 
